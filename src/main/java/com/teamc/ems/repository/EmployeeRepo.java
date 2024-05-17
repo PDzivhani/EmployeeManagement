7b204cde50e4fc5d1a43d262c16cd888fe39684e
Andile <andile.msomi64@gmail.com>
2024-05-17 07:43:19 +0200

    1stComm

--- src/main/java/com/teamc/ems/repository/EmployeeRepo.java
+++ src/main/java/com/teamc/ems/repository/EmployeeRepo.java
@@ -1,8 +1,10 @@
 package com.teamc.ems.repository;
 
 import com.teamc.ems.entity.Employees;
+import com.teamc.ems.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepo extends JpaRepository<Employees, Long> {
+import java.util.Optional;
 
+public interface EmployeeRepo extends JpaRepository<Employees, Long> {
 }
