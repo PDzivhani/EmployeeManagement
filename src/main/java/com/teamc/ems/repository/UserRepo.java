c5f68fd00237b8a46343b48322d79e473c1f40cf
Andile <andile.msomi64@gmail.com>
2024-05-17 08:08:35 +0200

    JWT

---
+++ src/main/java/com/teamc/ems/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.teamc.ems.repository;
+
+import com.teamc.ems.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
