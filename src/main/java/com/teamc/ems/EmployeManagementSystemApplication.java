f2c87a24ddfb3e4f3c113faabe6a885c76f0e940
Sifundo-B <sifundo.shenge12@gmail.com>
2024-05-23 10:32:55 +0200

    global cross origin

--- src/main/java/com/teamc/ems/EmployeManagementSystemApplication.java
+++ src/main/java/com/teamc/ems/EmployeManagementSystemApplication.java
@@ -2,6 +2,10 @@ package com.teamc.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeManagementSystemApplication {
@@ -9,5 +13,15 @@ public class EmployeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeManagementSystemApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) { registry.addMapping("/api/**")
+					.allowedOrigins("http://localhost:4200")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("*") .allowCredentials(true); } };
+		}
+	}
 
-}
+
