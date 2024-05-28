e337d060ee9f6c9581b6cb1ffc67af492e67e279
Andile <andile.msomi64@gmail.com>
2024-05-28 08:38:43 +0200

    added profile img update backend

--- src/main/java/com/teamc/ems/repository/EmployeeRepo.java
+++ src/main/java/com/teamc/ems/repository/EmployeeRepo.java
@@ -1,11 +1,18 @@
 package com.teamc.ems.repository;
 
 import com.teamc.ems.entity.EMPUser;
+import com.teamc.ems.service.EmployeesImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<EMPUser, Long> {
+    static EmployeesImpl save(EmployeesImpl employee) {
+        return employee;
+    }
+
     List<EMPUser>findByDeletedFalse();
 
 }
